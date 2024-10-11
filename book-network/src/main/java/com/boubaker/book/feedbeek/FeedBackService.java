ff4a0d3570f76352744bf9edbbced8e0529f6c0f
boubaker <belgecemjrad2018@gmail.com>
2024-10-11 06:48:49 +0200

    add findAllFeedbacksByBook method to feedbackController and its service method and repo method also creating new class FeedbackResponse and create new method in feedbackMapper toFeedbackResponse

--- book-network/src/main/java/com/boubaker/book/feedbeek/FeedBackService.java
+++ book-network/src/main/java/com/boubaker/book/feedbeek/FeedBackService.java
@@ -1,16 +1,21 @@
 package com.boubaker.book.feedbeek;
 
+import java.util.List;
 import java.util.Objects;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.boubaker.book.book.Book;
 import com.boubaker.book.book.BookRepository;
+import com.boubaker.book.common.PageResponse;
 import com.boubaker.book.exception.OperationNotPermittedException;
 import com.boubaker.book.user.User;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -41,6 +46,27 @@ public class FeedBackService {
             
             Feedback feedback = feedbackMapper.toFeedback(request);
             return feedBackRepository.save(feedback).getId();
+    }
+
+
+    @Transactional
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size);
+        User user = ((User) connectedUser.getPrincipal());
+        Page<Feedback> feedbacks = feedBackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+
     }
 
 }
--- book-network/src/main/java/com/boubaker/book/feedbeek/FeedbackMapper.java
+++ book-network/src/main/java/com/boubaker/book/feedbeek/FeedbackMapper.java
@@ -1,5 +1,7 @@
 package com.boubaker.book.feedbeek;
 
+import java.util.Objects;
+
 import com.boubaker.book.book.Book;
 
 public class FeedbackMapper {
@@ -15,6 +17,14 @@ public class FeedbackMapper {
                         .archived(false) // Not required and has no impact :: just to satisfy lombok
                         .build()
                 )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
                 .build();
     }
 }
