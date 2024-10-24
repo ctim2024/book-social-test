18f85a9e3cf5c7436d3df62ef21db16bdd1f4545
boubaker <belgecemjrad2018@gmail.com>
2024-10-24 09:16:02 +0200

    add code snippet in http-token interceptor to exlude some paths from adding a token

--- book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -6,7 +6,18 @@ import { inject } from '@angular/core';
 export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token:string = inject(TokenService).token;
-  
+  const excludedUrls = [
+    '/api/v1/auth/authenticate',
+    '/api/v1/auth/register',
+    '/api/v1/auth/activate-account'
+  ];
+
+    // Check if the request URL is in the excluded list
+  if (excludedUrls.some(url => req.url.includes(url))) {
+      // If the URL is excluded, skip the interceptor logic
+      return next(req);
+    }
+    
   if (token) {
 
     const authReq = req.clone({
