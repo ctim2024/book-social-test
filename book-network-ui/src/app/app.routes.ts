8751f82fac46d776e874e16ff64d4580c82fbd8d
boubaker <belgecemjrad2018@gmail.com>
2024-11-27 10:15:09 +0200

    add guard canActivate authGaurd for redirect to login age when token is expired or doesn't exist and it works well

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import { BookModule } from './modules/book/book.module';
+import { authGuard } from './services/guard/auth.guard';
 
 
 export const routes: Routes = [
@@ -11,6 +12,8 @@ export const routes: Routes = [
 { path: 'login',  component: LoginComponent },
 { path: 'register',  component: RegisterComponent },
 { path: 'activate-account',  component: ActivateAccountComponent },
-{ path: 'books', loadChildren: () => import('./modules/book/book.module').then (m => BookModule)}
+{ path: 'books', loadChildren: () => import('./modules/book/book.module').then (m => BookModule),
+    canActivate:[authGuard]
+}
 
 ];
--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
+
 
   set token(token:string){
        localStorage.setItem('token', token);
@@ -11,5 +12,25 @@ export class TokenService {
 
   get token(){
     return localStorage.getItem('token') as string;
+  }
+
+  isTokenNotValid():boolean {
+   
+   return !this.isTokenValid();
+  }
+  isTokenValid():boolean {
+    const token = this.token;
+    if(!token){
+      return false;
+    }
+
+    //decode the token
+    const jwtHelper:JwtHelperService = new JwtHelperService();
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if(isTokenExpired){
+      localStorage.clear();
+      return false;
+    }
+   return true;
   }
 }
